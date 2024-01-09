3f312ebbf6794d03f1f7bdcf1d92d62ebde14dda
Fedec96 <fedec96@gmail.com>
2024-01-09 11:29:21 +0100

    Update use-safe-url.ts

--- src/hooks/utils/use-safe-url/use-safe-url.ts
+++ src/hooks/utils/use-safe-url/use-safe-url.ts
@@ -3,7 +3,8 @@ import { useState, useEffect } from "react";
 import type { Endpoint } from "../../../types";
 import type { UseSafeUrlResult } from "./types";
 
-const __safe = (url: Endpoint): string => String(url);
+const __safe = (url: Endpoint): string =>
+  typeof url === "string" ? url : String(url);
 
 export const useSafeUrl = (url: Endpoint): UseSafeUrlResult => {
   const [endpoint, setEndpoint] = useState(__safe(url));
